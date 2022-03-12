4a38e8a507d7589d1c07ff29f4db2037029e997d
abhinandan-SS25 <abhinandan.ss252003@gmail.com>
2022-03-12 18:12:08 +0530

    url bug fix

--- mailer/urls.py
+++ mailer/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login", views.auth_login, name="login"),
-    path("logout", views.auth_logout, name="logout"),
-    path("register", views.auth_register, name="register"),
+    path("login", views.auth_login, name="auth_login"),
+    path("logout", views.auth_logout, name="auth_logout"),
+    path("register", views.auth_register, name="auth_register"),
     path("emails", views.compose, name="compose"),
     path("emails/<int:email_id>", views.email, name="email"),
     path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
