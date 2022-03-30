5eaa3bfe6637745643e0d0ab2347f8bcb70476f3
abhinandan-SS25 <abhinandan.ss252003@gmail.com>
2022-03-30 18:33:36 +0530

    Fixed bug for name recognition in group view

--- src/MailBox/Inbox/Views/Groupmail/GroupMail.js
+++ src/MailBox/Inbox/Views/Groupmail/GroupMail.js
@@ -60,7 +60,8 @@ const GroupMail = (props) => {
     },[])
 
     const network_accounts = group_accounts.map(account=>{
-        const is_user = account.email===props.user.username?true:false
+        const is_user = account.email.split("@")[0]===props.user.username.split("@")[0]?true:false
+        console.log(account.email.split("@")[0],props.user.username.split("@")[0])
         return !is_user && <div onClick={add_name} data-name={account.email} className="group_account">
             <div data-name={account.email} className="group_account_name">
                 {account.firstname} {account.lastname}
